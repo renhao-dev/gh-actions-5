ff0e6b6799641c4832811b865c75b9586de724e5
unknown <sam.haff.rn@gmail.com>
2024-11-02 14:45:39 +0200

    add testing both local and cloud db

--- data/database.js
+++ data/database.js
@@ -4,8 +4,9 @@ const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
 const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
+const dbProtocol = process.env.MONGO_DB_PROTOCOL_URL;
 
-const uri = `mongodb://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+const uri = `${dbProtocol}://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
 console.log('Trying to connect to db');
